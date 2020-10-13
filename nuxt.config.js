1d313609e720f82e8078fa530c3392b62dfbe976
dev-ro <dev.rohit256@gmail.com>
2020-10-13 14:23:56 +0530

    modifie

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,10 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-
+  server: {
+    port: 3001,
+    host: '0.0.0.0'
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'news-website',
